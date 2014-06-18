1b8de436fa27921ce0cc06d8225a3953ad38d3e5
Zane Yao <yaoazhen@gmail.com>
2014-06-18 18:03:47 +0800

    add svgmin to reduce size for svg and data-uri

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,30 @@ module.exports = function (grunt) {
     pkg: grunt.file.readJSON('package.json'),
 
     clean: {
+      tmp:'.tmp/',
       dist: 'output/'
+    },
+    svgmin: {
+      options: {
+        plugins: [
+          { removeViewBox: false },
+          { removeUselessStrokeAndFill: false }
+        ]
+      },
+      ui: {                        // Target
+        files: [{                  // Dictionary of files
+          expand: true,            // Enable dynamic expansion.
+          cwd: 'source',  // Src matches are relative to this path.
+          src: ['**/*.svg'],       // Actual pattern(s) to match.
+          dest: '.tmp/source', // Destination path prefix.
+          ext: '.svg'              // Dest filepaths will have this extension.
+          // ie: optimise img/src/branding/logo.svg and store it in img/branding/logo.min.svg
+        }]
+      }
     },
     webfont: {
       icons: {
-        src: 'source/*.svg',
+        src: '.tmp/source/*.svg',
         destCss: 'output/grunt-webfont/css',
         dest: 'output/grunt-webfont/fonts'
       }
@@ -18,7 +37,7 @@ module.exports = function (grunt) {
         files: [
           {
             expand: true,
-            cwd: 'source',
+            cwd: '.tmp/source',
             src: ['*.svg', '*.png'],
             dest: "output/grunticon"
           }
@@ -30,12 +49,15 @@ module.exports = function (grunt) {
   });
 
   grunt.loadNpmTasks('grunt-webfont');
+  grunt.loadNpmTasks('grunt-svgmin');
   grunt.loadNpmTasks('grunt-grunticon');
   grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.registerTask("default", [
     'clean',
+    'svgmin',
     'webfont',
-    'grunticon'
+    'grunticon',
+    'clean:tmp'
   ]);
 };
